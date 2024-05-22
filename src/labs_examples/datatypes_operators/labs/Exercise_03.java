13e11389a1515e2d4c6686fcb22636aa17b9e10f
Lucas Brist <lucastbrist@gmail.com>
2024-05-22 14:41:19 -0500

    completed exercises

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,37 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int a = 10;
+        int b = 15;
+        int sum1 = (a + b);
+        System.out.println("int a is " +a);
+        System.out.println("int b is " +b);
+        System.out.println("a plus b is " +sum1);
+
+        int c = 2;
+        int d = 1;
+        int sum2 = (c - d);
+        System.out.println("int c is " +c);
+        System.out.println("int d is " +d);
+        System.out.println("d minus e is " + sum2);
+
+        int e = 4;
+        double f = 1.5;
+        System.out.println("int e is " +e);
+        System.out.println("double f is " +f);
+        System.out.println("e times f is " +(e * f));
+
+        int g = 7;
+        double h = 3.5;
+        System.out.println("int g is " +g);
+        System.out.println("double h is " +h);
+        System.out.println("g divided by h is " +(g / h));
+
+        int i = 10;
+        int j = 5;
+        System.out.println("int i is " +i);
+        System.out.println("int j is " +j);
+        System.out.println("i % j is " +(i % j));
 
     }
 
