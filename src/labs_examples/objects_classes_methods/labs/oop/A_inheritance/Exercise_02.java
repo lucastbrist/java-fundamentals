b775fbb6d36de95d71a3362543691e16c461050d
Lucas Brist <lucastbrist@gmail.com>
2024-07-21 13:35:56 -0500

    completed exercise

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
- *
+ * Because the sout line calls for the variable in the super class (aDOTi), not the variable as it is in the subclass.
  */
 class A {
     int i = 10;
