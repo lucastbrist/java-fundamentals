ded03014c0b570ca671d29651b845bdbfbe9bcd3
Lucas Brist <lucastbrist@gmail.com>
2024-08-14 16:11:15 -0500

    finished exercises

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -9,10 +11,24 @@ package labs_examples.input_output.labs;
  *
  */
 
-class Example {
+class Exercise_01 {
     public static void main(String[] args) {
 
+        String readPath = "src/labs_examples/input_output/files/byte_data";
+        String writePath = "src/labs_examples/input_output/files/byte_data_out";
 
+        try (BufferedInputStream fin = new BufferedInputStream(new FileInputStream(readPath));
+             BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream(writePath))) {
+
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = fin.read(buffer)) != -1) {
+                fout.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -12,3 +14,44 @@ package labs_examples.input_output.labs;
  *
  */
 
+class Exercise_02 {
+    public static void main(String[] args) {
+
+        String readPath = "src/labs_examples/input_output/files/char_data.txt";
+        String writePath = "src/labs_examples/input_output/files/char_data_out.txt";
+
+        try (BufferedReader fin = new BufferedReader(new FileReader(readPath));
+             BufferedWriter fout = new BufferedWriter(new FileWriter(writePath))) {
+
+            int ch;
+
+            while ((ch = fin.read()) != -1) {
+                if (ch == 'a') {
+                    ch = '-';
+                } else if (ch == 'e') {
+                    ch = '/';
+                }
+                fout.write(ch);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try (BufferedReader fin = new BufferedReader(new FileReader(writePath))) {
+
+            int ch;
+
+            while ((ch = fin.read()) != -1) {
+                if (ch == '-') {
+                    ch = 'a';
+                } else if (ch == '/') {
+                    ch = 'e';
+                }
+                System.out.print((char) ch);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
